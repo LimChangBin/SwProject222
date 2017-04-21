c0b6823000e4e0abd541a36807d2cd1a4c1df458
KwakDongHoon <kdhsuperman@kw.ac.kr>
2017-04-21 11:11:34 +0900

    C project

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,3 +1,16 @@
+#if(defined(_WIN32) || defined(_WIN64))
+#define SR_WIN 1
+#else
+#define SR_WIN 0
+#endif
+
+#if(!SR_WIN)
+#define SR_ANDROID 1
+#else
+#define SR_ANDROID 0
+#endif
+
+#if(SR_ANDROID)
 #include <jni.h>
 #include <string>
 
@@ -9,3 +22,6 @@ Java_com_example_engineering_we_swproject_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+#elif (SR_WIN)
+
+#endif
